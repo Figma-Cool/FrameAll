5b61b6750a1e165292d0e103fea96553ed5a6a85
Yancy Min <senseslofter@gmail.com>
2020-10-08 23:41:12 +0800

    Update code.js

--- code.js
+++ code.js
@@ -1,14 +1,33 @@
 const selection = figma.currentPage.selection;
 
-selection.map(item => {
-    let frame = figma.createFrame();
-    frame.name = item.name;
-    frame.resize(item.width, item.height)
-    frame.x = item.x;
-    frame.y = item.y;
-    frame.appendChild(item);
-    item.x = 0;
-    item.y = 0;
+selection.forEach(item => {
+    if (item.parent.type === "FRAME") {
+        // console.log('frame')
+        let frame = figma.createFrame();
+        let x = item.x;
+        let y = item.y;
+        frame.name = item.name;
+        frame.fills = [];
+        frame.resize(item.width, item.height)
+        item.parent.appendChild(frame);
+        frame.x = x;
+        frame.y = y;
+        frame.appendChild(item);
+        item.x = 0;
+        item.y = 0;
+    }
+    if (item.parent.type === "PAGE") {
+        // console.log('page')
+        let frame = figma.createFrame();
+        frame.name = item.name;
+        frame.fills = [];
+        frame.resize(item.width, item.height)
+        frame.x = item.x;
+        frame.y = item.y;
+        frame.appendChild(item);
+        item.x = 0;
+        item.y = 0;
+    }
 })
 
-figma.closePlugin();
+figma.closePlugin("👌Done");
